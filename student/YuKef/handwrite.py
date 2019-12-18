2d50f04d81f6ee69d878921af08ec7acc7e05f8e
Yu Kef <56345868+UDexerve@users.noreply.github.com>
2019-12-18 11:22:05 +0800

    Add files via upload

---
+++ student/YuKef/handwrite.py
@@ -0,0 +1,499 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+import tensorflow as tf
+
+
+# In[2]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (7,7) # Make the figures a bit bigger
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.utils import np_utils
+
+
+# In[3]:
+
+
+
+nb_classes = 12
+
+# the data, shuffled and split between tran and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+print("X_train original shape", X_train.shape)
+print("y_train original shape", y_train.shape)
+print("X_test0  original shape", X_test[0].shape)
+
+
+# In[4]:
+
+
+
+for i in range(9):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_train[i], cmap='gray', interpolation='none')
+    plt.title("Class {}".format(y_train[i]))
+    
+
+
+# In[5]:
+
+
+for line in X_train[0]:
+    linestr=''
+    for pix in line:
+        linestr+=str(pix)+' '
+    print(linestr) 
+
+for i in range(32):
+    plt.subplot(8,4,i+1)
+    plt.imshow(X_train[i], cmap='gray', interpolation='none')
+    plt.title("Class {}".format(y_train[i]))
+
+print(y_train)
+
+
+# In[6]:
+
+
+import cv2
+Xtrain0=X_train[0]
+cv2.imshow("mw",Xtrain0)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[7]:
+
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print("Training matrix shape", X_train.shape)
+print("Testing matrix shape", X_test.shape)
+
+
+# In[8]:
+
+
+#print(X_train[0])
+
+linestr=''
+for i in range(len(X_train[0])):
+    j=i%28
+    if(j==0):
+        print(linestr)
+        linestr=""
+    linestr+="%.1f"%X_train[0][i]+" "
+
+
+# In[9]:
+
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+print(len(Y_train))
+print(len(Y_train[0]))
+print(Y_train)
+print(Y_test)
+
+
+# In[10]:
+
+
+model = Sequential()
+model.add(Dense(512, input_shape=(784,)))
+model.add(Activation('relu')) # An "activation" is just a non-linear function applied to the output
+                              # of the layer above. Here, with a "rectified linear unit",
+                              # we clamp all values below 0 to 0.
+                           
+model.add(Dropout(0.2))   # Dropout helps protect the model from memorizing or "overfitting" the training data
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax')) # This special "softmax" activation among other things,
+                                 # ensures the output is a valid probaility distribution, that is
+                                 # that its values are all non-negative and sum to 1.
+
+
+# In[11]:
+
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+
+# In[12]:
+
+
+
+model.fit(X_train, Y_train,
+          batch_size=128, epochs=4,verbose=1,
+          validation_data=(X_test, Y_test))
+
+
+# In[13]:
+
+
+score = model.evaluate(X_test, Y_test,verbose=1)
+print(score)
+
+
+# In[14]:
+
+
+# The predict_classes function outputs the highest probability class
+# according to the trained classifier for each input example.
+predicted_classes = model.predict_classes(X_test)
+
+print(predicted_classes)
+print(len(predicted_classes))
+
+# Check which items we got right / wrong
+predictyes=(predicted_classes == y_test)
+print(predictyes)
+
+predictnum=np.nonzero(predictyes)
+print(predictnum)
+
+correct_indices = np.nonzero(predicted_classes == y_test)[0]
+incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
+
+print("correct",correct_indices)
+print("incorrect",incorrect_indices)
+
+
+# In[15]:
+
+
+plt.figure()
+for i, correct in enumerate(correct_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[correct].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[correct], y_test[correct]))
+    
+plt.figure()
+for i, incorrect in enumerate(incorrect_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], y_test[incorrect]))
+
+
+# In[17]:
+
+
+import cv2
+import numpy as np
+
+img=cv2.imread("C:\\Users\\yukef\\Pictures\\Camera Roll\\handnumbers.jpg")
+#cv2.imshow("mw",img)
+#cv2.waitKey(0)
+W=290
+L=455
+NW=280
+NL=280
+X0=125
+Y0=160
+for i in range(2):
+    for j in range(4):
+        imdig=img[X0+L*i:X0+L*i+NL,Y0+W*j:Y0+W*j+NW]
+        cv2.imshow("imdig",imdig)
+        filename="hn"+str(i)+str(j)+".jpg"
+        cv2.rectangle(img,(Y0+W*j,X0+L*i),(Y0+W*j+NW,X0+L*i+NL),(0,255,0),3)        
+        cv2.imwrite(filename,imdig)
+        cv2.waitKey(0)
+cv2.imshow("img",img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+# In[18]:
+
+
+import cv2  
+
+for i in range(2):
+    for j in range(4):
+        filename="hn"+str(i)+str(j)+".jpg"
+        img = cv2.imread(filename)
+        GrayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
+        ret,thresh1=cv2.threshold(GrayImage,120,255,cv2.THRESH_BINARY)  
+        resized_image = cv2.resize(thresh1, (28, 28)) 
+        invimg = ~resized_image
+        outputfile="hn28inv"+str(i)+str(j)+".jpg"
+        print(outputfile)
+        cv2.imwrite(outputfile,invimg)
+
+
+# In[19]:
+
+
+import cv2
+img = cv2.imread("hn28inv11.jpg")
+print(len(img))
+print(len(img[0]))
+cv2.imshow("hninv",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[20]:
+
+
+print("x to be predicted original shape", img.shape)
+GrayImg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+print("x to be predicted original shape", GrayImg.shape)
+plt.imshow(GrayImg, cmap='gray', interpolation='none')
+
+
+# In[21]:
+
+
+X_pred = GrayImg.reshape(1, 784)
+X_pred = X_pred.astype('float32')
+X_pred /= 255
+print("Predicating matrix shape", X_pred.shape)
+
+
+# In[22]:
+
+
+predicted_classes = model.predict_classes(X_pred)
+
+print(predicted_classes)
+
+
+# In[23]:
+
+
+
+import cv2
+import numpy
+
+X_pred = numpy.zeros(shape=(8,28,28))
+index=0
+
+for i in range(2):
+    for j in range(4):
+        filename="hn28inv"+str(i)+str(j)+".jpg"
+        img = cv2.imread(filename)
+        GrayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        X_pred[index]=cv2.resize(GrayImage, (28, 28))
+        index+=1
+print(X_pred.shape)
+
+
+# In[24]:
+
+
+for i in range(8):
+    plt.subplot(2,4,i+1)
+    plt.imshow(X_pred[i], cmap='gray', interpolation='none')
+
+
+# In[25]:
+
+
+
+X_pred = X_pred.reshape(8, 784)
+X_pred = X_pred.astype('float32')
+X_pred /= 255
+print("Testing matrix shape", X_pred.shape)
+predicted_classes = model.predict_classes(X_pred)
+print(predicted_classes)
+
+
+# In[26]:
+
+
+model.save("ministxkn.h5")
+
+
+# In[27]:
+
+
+del model
+
+
+# In[28]:
+
+
+try:
+    predicted_classes = model.predict_classes(X_pred)
+except:
+    print("model not found, you have deleted it on purpose.")
+
+
+# In[29]:
+
+
+
+from keras.models import load_model
+
+model = load_model('ministxkn.h5')
+
+
+# In[30]:
+
+
+
+predicted_classes = model.predict_classes(X_pred)
+print(predicted_classes)
+
+
+# In[31]:
+
+
+
+print(X_train.shape)
+print(X_pred.shape)
+
+
+# In[32]:
+
+
+
+print(X_train[0:8])
+print(X_pred[0:8])
+
+
+# In[33]:
+
+
+X_train[0:8]=X_pred
+
+
+# In[34]:
+
+
+print(X_train[0:8])
+
+
+# In[35]:
+
+
+
+for i in range(8):
+    y_train[i]=i
+
+
+# In[36]:
+
+
+print(y_train[0:10])
+
+
+# In[37]:
+
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+
+
+# In[38]:
+
+
+print(Y_train[0:10])
+
+
+# In[39]:
+
+
+
+model.fit(X_train, Y_train,
+          batch_size=128, epochs=4,verbose=1,
+          validation_data=(X_test, Y_test))
+
+
+# In[40]:
+
+
+
+predicted_classes = model.predict_classes(X_pred)
+print(predicted_classes)
+
+
+# In[42]:
+
+
+import cv2
+import numpy as np
+
+img=cv2.imread("C:\\Users\\yukef\\Pictures\\Camera Roll\handnumbers2.jpg")
+imshow=img.copy()
+#cv2.imshow("mw",img)
+#cv2.waitKey(0)
+W=375
+L=490
+NW=375
+NL=490
+X0=0
+Y0=0
+for i in range(2):
+    for j in range(4):
+        x1=X0+L*i
+        y1=Y0+W*j
+        x2=x1+NL
+        y2=y1+NW
+        imdig=img[x1:x2,y1:y2]
+        cv2.imshow("imdig",imdig)
+        cv2.waitKey()
+        cv2.rectangle(imshow,(y1,x1),(y2,x2),(0,255,0),3)
+        filename="hn"+str(i)+str(j)+".jpg"
+        cv2.imwrite(filename,imdig)
+cv2.imshow("imdigall",imshow)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+for i in range(2):
+    for j in range(4):
+        filename="hn"+str(i)+str(j)+".jpg"
+        img = cv2.imread(filename)
+        GrayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
+        ret,thresh1=cv2.threshold(GrayImage,100,255,cv2.THRESH_BINARY)  
+        resized_image = cv2.resize(thresh1, (28, 28)) 
+        invimg = ~resized_image
+        outputfile="hn28inv"+str(i)+str(j)+".jpg"
+        print(outputfile)
+        cv2.imwrite(outputfile,invimg)
+
+X_pred = numpy.zeros(shape=(8,28,28))
+index=0
+
+for i in range(2):
+    for j in range(4):
+        filename="hn28inv"+str(i)+str(j)+".jpg"
+        img = cv2.imread(filename)
+        GrayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        X_pred[index]=cv2.resize(GrayImage, (28, 28))
+        index+=1
+print(X_pred.shape)
+
+for i in range(8):
+    plt.subplot(2,4,i+1)
+    plt.imshow(X_pred[i], cmap='gray', interpolation='none')
+    
+X_pred = X_pred.reshape(8, 784)
+X_pred = X_pred.astype('float32')
+X_pred /= 255
+print("Testing matrix shape", X_pred.shape)
+predicted_classes = model.predict_classes(X_pred)
+print(predicted_classes)
+
+
+# In[ ]:
+
+
+
+
