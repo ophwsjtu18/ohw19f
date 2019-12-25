c3fbe1a628d701862343c50e6b032589413c22da
terenceyjh <43612900+terenceyjh@users.noreply.github.com>
2019-12-25 18:12:54 +0800

    Update oo_pratice.py

--- student/jhyang/oo_pratice.py
+++ student/jhyang/oo_pratice.py
@@ -1,43 +1,59 @@
 import mcpi.minecraft as minecraft
-
 mc = minecraft.Minecraft.create()
+pos = mc.player.getTilePos()
+position=list(pos)
+position1=list(range(3))
+position2=list(range(3))
+
+for i in range(3):
+    position1[i]=list(pos)[i]+25
+    position2[i]=list(pos)[i]-25
+
+position1[1]=list(pos)[1]
+position2[1]=list(pos)[1]
 
 class House():
-    def __init__(self, data):
-        self.data = data 
-        self.pos = data
-
-    def getPos(self):
-        pos = mc.player.getTilePos()
-        self.pos[0]=self.pos[0]+pos[0]
-        self.pos[1]=self.pos[1]+pos[1]
-        self.pos[2]=self.pos[2]+pos[2]
-        #print(self.pos) 
-    def buildRoof(self):
+    def __init__(self,data):
+        self.data=data
+    def roof(self):
         x0=self.data[0]
         y0=self.data[1]
         z0=self.data[2]
-        for x in range(9):
-            for y in range(9):
-                mc.setBlock(x0+x,y0+y,z0,25)
+        for x in range(11):
+            for z in range(11):
+                mc.setBlock(x0+x,y0+8,z+z0,57)
+                    
+    def wall(self):
+        x0=self.data[0]
+        y0=self.data[1]
+        z0=self.data[2]
+        for y in range(9):
+            for z in range(11):
+                mc.setBlock(x0,y+y0,z+z0,41)
+                mc.setBlock(x0+10,y+y0,z+z0,41)
+        for y in range(9):
+            for x in range(10):
+                mc.setBlock(x0+x+1,y+y0,z0,41)
+                mc.setBlock(x0+x+1,y+y0,z0+10,41)
+                    
+    def door(self):
+        x0=self.data[0]
+        y0=self.data[1]
+        z0=self.data[2]
+        for y in range(5):
+            mc.setBlock(x0+4,y+y0,z0,0)
+            mc.setBlock(x0+5,y+y0,z0,0)
+
+    def window(self):
+        x0=self.data[0]
+        y0=self.data[1]
+        z0=self.data[2]
+        for y in range(2):
+            mc.setBlock(x0+7,y+y0+4,z0,20)
+            mc.setBlock(x0+8,y+y0+4,z0,20)
     
-    def buildColumn(self):
-        x0=self.data[0]
-        y0=self.data[1]
-        z0=self.data[2]
-        for z in range(z0-1):
-            mc.setBlock(x0+4,y0+4,z,25)
-            mc.setBlock(x0+5,y0+5,z,25)
-'''
-    def buildAll(self):
-        buildRoof(self)
-        buildAll(self)
-
-mh1 = House([100,200,50])
-mh1.buildRoof()
-
-mh2 = House([120,180,50])
-mh2.buildColumn()
-'''
-mh3 = House([150,150,50])
-mh3.getPos()
+mh=House(position)
+mh.wall()
+mh.roof()
+mh.door()
+mh.window()
